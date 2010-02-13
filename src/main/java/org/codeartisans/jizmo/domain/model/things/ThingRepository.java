c8d3aebe122838941739ecf27a0fad8549193d8b
Paul MERLIN <p.merlin@netheos.net>
2010-02-13 17:35:17 +0100

    ThingRepository: naive query implementations, need to test that

--- src/main/java/org/codeartisans/jizmo/domain/model/things/ThingRepository.java
+++ src/main/java/org/codeartisans/jizmo/domain/model/things/ThingRepository.java
@@ -21,12 +21,16 @@
  */
 package org.codeartisans.jizmo.domain.model.things;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.codeartisans.jizmo.domain.model.values.PayloadValue;
 import static org.qi4j.api.query.QueryExpressions.*;
 import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.mixin.Mixins;
 import org.qi4j.api.query.Query;
 import org.qi4j.api.query.QueryBuilder;
 import org.qi4j.api.query.QueryBuilderFactory;
+import org.qi4j.api.query.grammar.BooleanExpression;
 import org.qi4j.api.service.ServiceComposite;
 import org.qi4j.api.unitofwork.UnitOfWork;
 import org.qi4j.api.unitofwork.UnitOfWorkFactory;
@@ -35,6 +39,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * TODO Test each method!
+ * 
  * @author Paul Merlin <p.merlin@nosphere.org>
  */
 @Mixins( ThingRepository.Mixin.class )
@@ -46,7 +52,15 @@ public interface ThingRepository
 
     Query<ThingEntity> findAll();
 
+    Query<ThingEntity> findByIlk( @NotEmpty String ilk );
+
+    Query<ThingEntity> findByIlks( @NotEmpty String... ilks ); // OR
+
     Query<ThingEntity> findByTag( @NotEmpty String tag );
+
+    Query<ThingEntity> findByTags( @NotEmpty String... tags ); // AND
+
+    Query<ThingEntity> findByPayloadMimetype( @NotEmpty String mimetype );
 
     abstract class Mixin
             implements ThingRepository
@@ -68,8 +82,41 @@ public interface ThingRepository
         @Override
         public Query<ThingEntity> findAll()
         {
+            LOGGER.debug( "findByAll()" );
             UnitOfWork uow = uowf.currentUnitOfWork();
             QueryBuilder<ThingEntity> queryBuilder = qbf.newQueryBuilder( ThingEntity.class );
+            return queryBuilder.newQuery( uow );
+        }
+
+        @Override
+        public Query<ThingEntity> findByIlk( String ilk )
+        {
+            LOGGER.debug( "findByIlk(" + ilk + ")" );
+            UnitOfWork uow = uowf.currentUnitOfWork();
+            QueryBuilder<ThingEntity> queryBuilder = qbf.newQueryBuilder( ThingEntity.class );
+            IlkEntity ilkTemplate = templateFor( IlkEntity.class );
+            queryBuilder = queryBuilder.where(
+                    and( eq( ilkTemplate.name(), ilk ),
+                         eq( templateFor( ThingEntity.class ).ilk(), ilkTemplate ) ) );
+            return queryBuilder.newQuery( uow );
+        }
+
+        @Override
+        public Query<ThingEntity> findByIlks( String... ilks )
+        {
+            LOGGER.debug( "findByIlks(" + ilks + ")" );
+            UnitOfWork uow = uowf.currentUnitOfWork();
+            QueryBuilder<ThingEntity> queryBuilder = qbf.newQueryBuilder( ThingEntity.class );
+            ThingEntity thingTemplate = templateFor( ThingEntity.class );
+            List<BooleanExpression> bools = new ArrayList<BooleanExpression>();
+            for ( String eachIlk : ilks ) {
+                IlkEntity ilkTemplate = templateFor( IlkEntity.class );
+                bools.add( and( eq( ilkTemplate.name(), eachIlk ),
+                                eq( thingTemplate.ilk(), ilkTemplate ) ) );
+            }
+            BooleanExpression first = bools.remove( 0 );
+            BooleanExpression second = bools.remove( 0 );
+            queryBuilder = queryBuilder.where( or( first, second, bools.toArray( new BooleanExpression[ bools.size() ] ) ) );
             return queryBuilder.newQuery( uow );
         }
 
@@ -83,6 +130,34 @@ public interface ThingRepository
             queryBuilder = queryBuilder.where(
                     and( eq( tagTemplate.name(), tag ),
                          contains( templateFor( ThingEntity.class ).tags(), tagTemplate ) ) );
+            return queryBuilder.newQuery( uow );
+        }
+
+        @Override
+        public Query<ThingEntity> findByTags( String... tags )
+        {
+            LOGGER.debug( "findByTags(" + tags + ")" );
+            UnitOfWork uow = uowf.currentUnitOfWork();
+            QueryBuilder<ThingEntity> queryBuilder = qbf.newQueryBuilder( ThingEntity.class );
+            for ( String eachTag : tags ) {
+                TagEntity tagTemplate = templateFor( TagEntity.class );
+                queryBuilder = queryBuilder.where(
+                        and( eq( tagTemplate.name(), eachTag ),
+                             contains( templateFor( ThingEntity.class ).tags(), tagTemplate ) ) );
+            }
+            return queryBuilder.newQuery( uow );
+        }
+
+        @Override
+        public Query<ThingEntity> findByPayloadMimetype( String mimetype )
+        {
+            LOGGER.debug( "findByPayloadMimetype(" + mimetype + ")" );
+            UnitOfWork uow = uowf.currentUnitOfWork();
+            QueryBuilder<ThingEntity> queryBuilder = qbf.newQueryBuilder( ThingEntity.class );
+            PayloadValue payloadTemplate = templateFor( PayloadValue.class );
+            queryBuilder = queryBuilder.where(
+                    and( eq( payloadTemplate.mimeType(), mimetype ),
+                         contains( templateFor( ThingEntity.class ).payloads(), payloadTemplate ) ) );
             return queryBuilder.newQuery( uow );
         }
 
